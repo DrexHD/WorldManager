b6e41e92ec4a3db0435f777d195e139195b05553
Drex <nicknamedrex@gmail.com>
2025-07-24 12:10:58 +0200

    Update to 1.21.8

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("dev.kikugie.stonecutter")
     id("org.jetbrains.changelog") version "2.2.1"
 }
-stonecutter active "1.21.6"
+stonecutter active "1.21.8"
 
 stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) { 
     group = "project"
