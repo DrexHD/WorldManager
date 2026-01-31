663ff17ca608c8e95b9e938af5a1307ec6b35ed9
Drex <nicknamedrex@gmail.com>
2026-01-31 01:14:48 +0100

    Improve last location logic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.4.1] - 2026-01-31
+### Changed
+- Improved last location logic
+
 ### Fixed
 - Error when setting portal behavior on a new world
 
--- src/main/java/me/drex/worldmanager/mixin/ServerPlayerMixin.java
+++ src/main/java/me/drex/worldmanager/mixin/ServerPlayerMixin.java
@@ -6,39 +6,27 @@ import me.drex.worldmanager.data.PlayerData;
 import me.drex.worldmanager.save.Location;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.portal./*? if >=1.21.2 {*/ TeleportTransition /*?} else {*/ /*DimensionTransition *//*?}*/;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collections;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
     @Shadow
-    public abstract ServerLevel /*? if >= 1.21.6-pre1 {*/ level() /*?} else {*/ /*serverLevel() *//*?}*/;
+    public abstract ServerLevel /*? if >= 1.21.6 {*/ level() /*?} else {*/ /*serverLevel() *//*?}*/;
 
-    @Inject(
-        //? if >= 1.21.2 {
-        method = "teleport(Lnet/minecraft/world/level/portal/TeleportTransition;)Lnet/minecraft/server/level/ServerPlayer;",
-        //?} else {
-        /*method = "changeDimension",
-        *///?}
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/server/level/ServerPlayer;setServerLevel(Lnet/minecraft/server/level/ServerLevel;)V"
-        )
-    )
-    public void saveLocation(/*? if >=1.21.2 {*/ TeleportTransition /*?} else {*/ /*DimensionTransition *//*?}*/ dimensionTransition, CallbackInfoReturnable<Entity> cir) {
+    @Inject(method = "doTick", at = @At("HEAD"))
+    public void saveLocation(CallbackInfo ci) {
         var player = (ServerPlayer) (Object) this;
         PlayerData playerData = PlayerDataApi.getCustomDataFor(player, WorldManager.STORAGE);
         if (playerData == null) {
             playerData = new PlayerData(Collections.emptyMap());
             PlayerDataApi.setCustomDataFor(player, WorldManager.STORAGE, playerData);
         }
-        playerData.locations().put(/*? if >= 1.21.6-pre1 {*/ level() /*?} else {*/ /*serverLevel() *//*?}*/.dimension(), new Location(player));
+        playerData.locations().put(/*? if >= 1.21.6 {*/ level() /*?} else {*/ /*serverLevel() *//*?}*/.dimension(), new Location(player));
     }
 }
